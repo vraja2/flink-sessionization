9ee3f034a2c19db4e8bc4591d12e7b4a69f2ecff
Vignesh Raja <vignesh@optimizely.com>
2018-09-22 23:49:17 -0700

    Using timer to flush sessions periodically from RocksDB

--- src/main/java/com/vigneshraja/sessionization/ComputeSessions.java
+++ src/main/java/com/vigneshraja/sessionization/ComputeSessions.java
@@ -5,49 +5,61 @@ import com.vigneshraja.sessionization.models.ClickstreamEvent;
 import com.vigneshraja.sessionization.models.Session;
 import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
-import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 /**
- * Created by vraja on 9/4/18
+ * Created by vraja on 9/6/18
  */
-public class ComputeSessions extends ProcessWindowFunction<ClickstreamEvent, Session, String, GlobalWindow> {
+public class ComputeSessions extends KeyedProcessFunction<String, ClickstreamEvent, Session> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ComputeSessions.class);
     private transient MapState<String, Session> sessionState;
+    private transient ValueState<Long> timerState;
 
     @Override
     public void open(Configuration parameters) throws Exception {
         MapStateDescriptor<String, Session> descriptor =
             new MapStateDescriptor<>("session", TypeInformation.of(String.class), TypeInformation.of(Session.class));
         sessionState = getRuntimeContext().getMapState(descriptor);
+
+        ValueStateDescriptor<Long> timerStateDescriptor =
+            new ValueStateDescriptor<>("timerState", TypeInformation.of(Long.class));
+        timerState = getRuntimeContext().getState(timerStateDescriptor);
     }
 
     @Override
-    public void clear(Context context) throws Exception {
-        super.clear(context);
-    }
-
-    @Override
-    public void process(String s, Context context, Iterable<ClickstreamEvent> events, Collector<Session> out) throws Exception {
-        // scan through rocksDb
-        boolean sawElements = false;
-        for (Map.Entry<String, Session> entry : sessionState.entries()) {
-            LOG.info("ENTRY");
-            LOG.info(entry.getKey());
-            LOG.info(entry.getValue().toString());
-            out.collect(entry.getValue());
-            sawElements = true;
+    public void processElement(ClickstreamEvent event, Context ctx, Collector<Session> out) throws Exception {
+        // set the first timer
+        if (timerState.value() == null) {
+            // TODO: put timer interval in config
+            long nextTimer = System.currentTimeMillis() + 60000;
+            ctx.timerService().registerProcessingTimeTimer(nextTimer);
+            timerState.update(nextTimer);
         }
 
-        System.out.println("Saw elements " + sawElements);
-        LOG.info("Saw elements {}", sawElements);
+        if (sessionState.contains(event.getKey())) {
+            // TODO: if session is closed, replace with new session
+            sessionState.get(event.getKey()).getEvents().add(event);
+        } else {
+            sessionState.put(event.getKey(), new Session(Lists.newArrayList(event)));
+        }
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<Session> out) throws Exception {
+        for (Map.Entry<String, Session> entry : sessionState.entries()) {
+            // TODO: if more than 30 minutes of inactivity, close session
+            out.collect(entry.getValue());
+        }
+
+        long nextTimer = System.currentTimeMillis() + 60000;
+        timerState.update(nextTimer);
+        ctx.timerService().registerProcessingTimeTimer(nextTimer);
     }
 }
--- src/main/java/com/vigneshraja/sessionization/Sessionization.java
+++ src/main/java/com/vigneshraja/sessionization/Sessionization.java
@@ -7,15 +7,9 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.triggers.ContinuousProcessingTimeTrigger;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by vraja on 9/2/18
@@ -40,20 +34,11 @@ public class Sessionization {
         StateBackend stateBackend = new RocksDBStateBackend("file:////Users/vraja/flink-checkpoint");
         env.setStateBackend(stateBackend);
 
-        // GlobalWindows assigner doesn't have default trigger unlike other window assigners
-        // clickstream will be grouped by userId
-        // process keyedStream and store in RocksDB state (use ProcessFunction)
-        // trigger defines when to process elements in window
-        KeyedStream<ClickstreamEvent, String> keyedClickStream = datastream.keyBy(ClickstreamEvent::getKey);
-
-        keyedClickStream.process(new PopulateState());
-
-        // process rocksdb state and for closed sessions push to kafka
-        SingleOutputStreamOperator<Session> sessionStream = keyedClickStream
-            .window(GlobalWindows.create())
-            .trigger(ContinuousProcessingTimeTrigger.of(Time.of(1, TimeUnit.MINUTES)))
+        SingleOutputStreamOperator<Session> sessionStream = datastream
+            .keyBy(ClickstreamEvent::getKey)
             .process(new ComputeSessions());
 
+        // just write to sdout for now
         sessionStream.map(Session::toString).print();
 
         env.execute();
